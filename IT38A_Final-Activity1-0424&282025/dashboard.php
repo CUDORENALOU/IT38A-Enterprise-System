fd3dee284a818a85cf9e5911ca9d06166e62102e
Angelodatoycabana <angelodatoycabana11@gmail.com>
2025-04-28 21:37:03 +0800

    Added side bar

---
+++ IT38A_Final-Activity1-0424&282025/dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Dashboard - HardwareHub</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="sidebar">
+    <div class="logo">
+        <img src="logo.jpg" alt="HardwareHub Logo">
+    </div>
+    <h2>HardwareHub</h2>
+    <a href="#">Dashboard</a>
+    <a href="#">Manage Products</a>
+    <a href="#">Orders</a>
+    <a href="#">Users</a>
+    <a href="#">Reports</a>
+    <a href="#">Settings</a>
+    <a href="#">Log Out</a>
+</div>
+
+</body>
+</html>
